24bda4d50e69f0bd98f54fee32dd04cafebee52b
ACC01092 <marrsantosjunior@gmail.com>
2019-08-27 18:56:28 -0300

    Setup App Environment  - Create Gemfile  - Create Podfile with the required tests dependencies

---
+++ QuizChallenge/QuizChallenge/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  QuizChallenge
+//
+//  Created by m.dos.santos.junior on 27/08/19.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
