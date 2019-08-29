42ea2707f5a5ad253766dab8ec2fcead304607d4
ACC01092 <marrsantosjunior@gmail.com>
2019-08-29 15:36:51 -0300

    Add testing app delegate

---
+++ QuizChallenge/QuizChallenge/Application/main.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let appDelegateClass: AnyClass =
+    NSClassFromString("TestingAppDelegate") ?? AppDelegate.self
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(appDelegateClass)
+)
